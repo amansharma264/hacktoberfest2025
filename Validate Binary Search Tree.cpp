836fb7412ab3690b980c33b930f43fdd1a73be59
Aman kumar <amankumar170789@gmail.com>
2025-10-22 02:32:44 +0530

    Create Validate Binary Search Tree.cpp

---
+++ Validate Binary Search Tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    TreeNode* prev = NULL;
+    bool flag = true;
+    void inorder(TreeNode* root){
+    if(root == NULL) return;
+    inorder(root->left);
+    if(prev != NULL){
+        if(root->val <= prev->val){
+            flag = false;
+            return;
+        }
+    }
+    prev = root;
+    inorder(root->right); // right
+}
+    bool isValidBST(TreeNode* root) {
+        inorder(root);
+        return flag;
+
+    }
+};
